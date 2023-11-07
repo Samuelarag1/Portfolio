76497cc4ca4e72aaf340b68cc9b9ad59d220fd28
Samuel Aragon <samuelaragon616@gmail.com>
2023-11-07 18:33:21 -0300

    no more ideas

--- server/app.js
+++ server/app.js
@@ -3,12 +3,10 @@ const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
 const path = require("path");
-const cors = require("cors");
 
 const PORT = 3001;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
-
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,8 +15,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-
-app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Starting server in PORT: ${PORT}`);
@@ -35,20 +31,21 @@ app.post("/postMessage", async (req, res) => {
       pass: process.env.USER_PASSWORD,
     },
   };
+
   const mensaje = {
     subject: subject,
     from: email,
     to: process.env.USER_EMAIL,
     text: text,
     html: `
-          <div style="font-family: 'Roboto', sans-serif; color: #333; background-color: #f9f9f9; padding: 20px;">
-            <h1 style="color: #007bff; text-align: center; font-size: 24px;">Este es un correo enviado desde el Portfolio!</h1>
-              <div style="margin-top: 20px;">
-                  <p style="font-size: 16px;"><strong>Nombre:</strong> ${subject}</p>
-                  <p style="font-size: 16px;"><strong>Correo de:</strong> ${email}</p>
-                  <p style="font-size: 16px;"><strong>Mensaje:</strong> ${text}</p>
-              </div>
-          </div>`,
+      <div style="font-family: 'Roboto', sans-serif; color: #333; background-color: #f9f9f9; padding: 20px;">
+        <h1 style="color: #007bff; text-align: center; font-size: 24px;">Este es un correo enviado desde el Portfolio!</h1>
+        <div style="margin-top: 20px;">
+          <p style="font-size: 16px;"><strong>Nombre:</strong> ${subject}</p>
+          <p style="font-size: 16px;"><strong>Correo de:</strong> ${email}</p>
+          <p style="font-size: 16px;"><strong>Mensaje:</strong> ${text}</p>
+        </div>
+      </div>`,
     importance: "high",
   };
 
