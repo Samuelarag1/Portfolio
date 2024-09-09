e5f2d5043f2dac49a20a9f441c0829c6e0ea4f90
Samuel Aragon <samuelaragon616@gmail.com>
2024-09-09 19:07:54 -0300

    update nav with RESUME

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -4,20 +4,25 @@ import { Link } from "react-scroll";
 const Nav = () => {
   return (
     <div className={styles.navegacion}>
-      <nav>
-        <Link to={"page-1"} smooth={true} offset={-100} duration={500}>
-          Inicio
-        </Link>
-        <Link to={"page-2"} smooth={true} offset={-99} duration={500}>
-          Skills
-        </Link>
-        <Link to={"page-3"} smooth={true} offset={-100} duration={500}>
-          Proyectos
-        </Link>
-        <Link to={"page-4"} smooth={true} offset={-100} duration={500}>
-          Contacto
-        </Link>
-      </nav>
+      <div className={styles.contenedor}>
+        <a href="/CV.pdf" download className={styles.cvButton}>
+          Descargar CV
+        </a>
+        <nav>
+          <Link to={"page-1"} smooth={true} offset={-100} duration={500}>
+            Inicio
+          </Link>
+          <Link to={"page-2"} smooth={true} offset={-99} duration={500}>
+            Skills
+          </Link>
+          <Link to={"page-3"} smooth={true} offset={-100} duration={500}>
+            Proyectos
+          </Link>
+          <Link to={"page-4"} smooth={true} offset={-100} duration={500}>
+            Contacto
+          </Link>
+        </nav>
+      </div>
     </div>
   );
 };
